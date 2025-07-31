6b2672cc8228ecce3320c08a3c319e0f24e1d44c
huudung <huudung2004kt@gmail.com>
2025-07-31 04:07:30 +0000

    remove module mailer

--- apps/admin/src/admin.module.ts
+++ apps/admin/src/admin.module.ts
@@ -5,10 +5,9 @@ import { CommonModule } from '@app/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from 'apps/auth/src/auth.module';
 import { TierModule } from './tier/tier.module';
-import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [CommonModule.forRoot(), UserModule, AuthModule, TierModule, MailModule],
+  imports: [CommonModule.forRoot(), UserModule, AuthModule, TierModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
